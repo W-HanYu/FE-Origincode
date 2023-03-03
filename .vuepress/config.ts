feed2a94d9cd5127c3a0d77f79a6b13f4c6b6f48
王涛 <wangtao@aimc.online>
2023-03-03 11:08:02 +0800

    origincode

---
+++ .vuepress/config.ts
@@ -0,0 +1,106 @@
+import { defineUserConfig } from "vuepress";
+import type { DefaultThemeOptions } from "vuepress";
+import recoTheme from "vuepress-theme-reco";
+
+export default defineUserConfig({
+  title: "Ustinian",
+  description: "Just playing around",
+  theme: recoTheme({
+    style: "@vuepress-reco/style-default",
+    logo: "/logo.png",
+    author: "wangtao",
+    authorAvatar: "/head.png",
+    // docsRepo: "https://github.com/vuepress-reco/vuepress-theme-reco-next",
+    docsBranch: "main",
+    docsDir: "example",
+    lastUpdatedText: "",
+    // series 为原 sidebar
+    series: {
+      "/docs/theme-reco/": [
+        {
+          text: "module one",
+          children: ["home", "theme"],
+        },
+        {
+          text: "module two",
+          children: ["api", "plugin"],
+        },
+      ],
+    },
+    navbar: [
+      { text: "首页", link: "/" },
+      { text: "React", link: "/categories/reco/1/" },
+      { text: "Vue", link: "/tags/tag1/1/" },
+      {
+        text: "小程序",
+        children: [
+          { text: "vuepress-reco", link: "/docs/theme-reco/theme" },
+          { text: "vuepress-theme-reco", link: "/blogs/other/guide" },
+        ],
+      },
+    ],
+    bulletin: {
+      body: [
+        {
+          type: "text",
+          content: `🎉🎉🎉React、Vue源码已更新，欢迎小伙伴们前来学习，一起讨论，共同进步，有问题的可以提Issues哈。`,
+          style: "font-size: 12px;",
+        },
+        {
+          type: "hr",
+        },
+        {
+          type: "title",
+          content: "QQ 群",
+        },
+        {
+          type: "text",
+          content: `
+          <ul>
+            <li>QQ群1：1037296104</li>
+          </ul>`,
+          style: "font-size: 12px;",
+        },
+        {
+          type: "hr",
+        },
+        {
+          type: "title",
+          content: "GitHub",
+        },
+        {
+          type: "text",
+          content: `
+          <ul>
+            <li><a href="https://github.com/W-HanYu/FE-Origincode/issues">Issues<a/></li>
+            <li><a href="https://github.com/W-HanYu/FE-Origincode/discussions/1">Discussions<a/></li>
+          </ul>`,
+          style: "font-size: 12px;",
+        },
+        {
+          type: "hr",
+        },
+        {
+          type: "buttongroup",
+          children: [
+            {
+              text: "打赏",
+              link: "/docs/others/donate.html",
+            },
+          ],
+        },
+      ],
+    },
+    // valineConfig 配置与 1.x 一致
+    // valineConfig: {
+    //   appId: 'xxx',
+    //   appKey: 'xxx',
+    //   placeholder: '填写邮箱可以收到回复提醒哦！',
+    //   verify: true, // 验证码服务
+    //   // notify: true,
+    //   recordIP: true,
+    //   // hideComments: true // 隐藏评论
+    // },
+  }),
+  // debug: true,
+});
